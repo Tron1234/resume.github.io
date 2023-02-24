7ee0934f6fdf74e49131a95902fb44281ea44ae9
“wangzhuohua” <506980247@qq.com>
2023-02-24 16:41:38 +0800

    first commit

---
+++ ZH_CN.js
@@ -0,0 +1,5 @@
+export const selfAssessment_title="自我总结"
+export const introduce=`我叫王卓华，25岁，目前就职岗位是前端开发工程师，使用的技术栈是vue、uniapp框架、微信小程序，主要从事移动端H5、微信公众号、混合App客户端的开发工作。善于处理web自适应问题、Ios、Android浏览器和App的兼容性问题，以及微信公众号JSSDK调用问题`
+export const selfAssessment=`从事这个行业已经有3年的时间了，在3年中做过独立开发多端项目、大部分服务端项目，有了一定的技术沉淀，也发现自身的一些技术问题，从会用到“下潜”深究原理。可能很多人认为前端开发没有技术难度，因为业务代码基本在后端，但对我一个从科班学后端最后走向前端的人来说，前端的魅力在于可以及时看到华丽的样式和动画效果，相比于无聊的大量数据，这才是我期待的`
+export const sendGift = `送你一个礼物，请向下滚动打开`
+export const peroration = `谢谢您的阅览，祝您每天幸福！`
---
+++ index.js
@@ -0,0 +1,311 @@
+import { debounce, throttle } from './utils.js'
+import { selfAssessment_title, introduce, selfAssessment, sendGift, peroration } from './ZH_CN.js'
+// 上一个动画完成的时间,第一页动画是否结束
+let lastTime = 0, isFirstEnd = false
+// 获取csss设置的变量值
+const root = document.querySelector(":root")
+
+function initAni() {
+  // 打字机
+  typing()
+  // 处理box
+  box()
+  // 处理事件
+  event()
+}
+
+// 打字机效果
+function typing() {
+  const introduce_dom = document.getElementById('introduce')
+  const title_dom = document.getElementById('title')
+  const selfAssessment_dom = document.getElementById('selfAssessment')
+  title_dom.innerText = selfAssessment_title
+  // 每个词需要的时间，单位秒
+  const wordTime = 0.1
+  //文本宽度
+  const fontSize = Number(getComputedStyle(root).getPropertyValue("--fontSize").replace('px', '')) + 2
+  // web端百分比
+  const webPercent = Number(getComputedStyle(root).getPropertyValue("--webPercent").replace('%', '')) / 100
+  // 移动端比例
+  const mobilePercent = Number(getComputedStyle(root).getPropertyValue("--mobilePercent").replace('%', '')) / 100
+  // 文本显示最长宽度
+  const maxWidth = document.documentElement.clientWidth * (window.screen.width > 500 ? webPercent : mobilePercent) - 34
+  // 柯里化写入dom
+  const hanlderChild = hanlderRow(fontSize, maxWidth)
+  hanlderChild(introduce_dom, introduce)
+  // 处理自我总结
+  root.style.setProperty('--lastRowWidth2', selfAssessment_title.length + 1 + 'em')
+  const time = selfAssessment_title.length * wordTime
+  title_dom.style = `animation: typing_last_2 ${time}s steps(${selfAssessment_title.length}, end) ${lastTime}s forwards, blink 0.6s step-end ${lastTime}s infinite, blink-close 0s ease ${lastTime + time}s forwards;`
+  setTimeout(() => {
+    document.getElementsByTagName('main')[0].style = 'overflow: hidden auto;height: auto;'
+    document.documentElement.scrollTo({
+      top: document.getElementById('title').offsetTop,
+      left: 0,
+      behavior: "smooth"
+    })
+  }, lastTime * 1000)
+  lastTime += time
+  hanlderChild(selfAssessment_dom, selfAssessment)
+
+  function hanlderRow(fontSize, maxWidth) {
+    let i = 0
+    return (dom, text) => {
+      i++
+      // 记录每一行文字数用于动画的时长计算
+      let length = 0 // 一行文字长度
+      let textIndex = 0 // 文字索引值
+      let lastIndex = 0 // 上一次的索引值
+      while (textIndex < text.length) {
+        const str = text[textIndex] // 当前文字
+        if (/[a-zA-Z0-9]/.test(str)) {
+          length += (fontSize / 1.5)
+        } else {
+          length += fontSize
+        }
+        // 计算除最后一行外的文本，最后一行在while循环外处理
+        if (length >= maxWidth) {
+          const str = text.slice(lastIndex, textIndex)
+          const time = str.length * wordTime
+          dom.appendChild(createDom('div', str, time, lastTime))
+          lastTime += time
+          lastIndex = textIndex
+          length = 0
+        }
+        textIndex++
+      }
+      const str = text.slice(lastIndex)
+      const time = str.length * wordTime
+      root.style.setProperty('--lastRowWidth' + (i === 1 ? 1 : 3), str.length + (i === 1 ? 2 : 4) + 'em')
+      dom.appendChild(createDom('div', str, time, lastTime, true))
+      lastTime += time
+    }
+
+    function createDom(tag, content, time, lastTime, last = false) {
+      const dom = document.createElement(tag)
+      dom.innerText = content
+      dom.style = `animation: ${last ? ('typing_last_' + (i === 1 ? 2 : 3)) : 'typing'} ${time}s steps(${content.length}, end) ${lastTime}s forwards, blink 0.6s step-end ${lastTime}s infinite, blink-close 0s ease ${lastTime + time}s forwards;`
+      return dom
+    }
+  }
+}
+
+function box() {
+  setTimeout(() => {
+    document.getElementsByClassName('gift-box')[0].style = 'opacity:1'
+    document.getElementsByClassName('control-box')[0].style = 'opacity:1'
+    isFirstEnd = true
+  }, lastTime * 1000)
+  document.getElementById('giftTitle').innerText = sendGift
+  document.getElementById('peroration').innerText = peroration
+}
+
+function event() {
+  const dom = document.querySelector('.gift>ul>li:nth-of-type(1)')
+  if (window.addEventListener) {
+    /** DOMMouseScroll is for mozilla. */
+    window.addEventListener('DOMMouseScroll', wheel, false)
+  }
+  window.onmousewheel = document.onmousewheel = wheel
+  window.ontouchstart = document.ontouchstart = touchstart
+  window.ontouchend = document.ontouchend = touchend
+
+  const distance = Number(getComputedStyle(root).getPropertyValue("--giftbox").replace('px', ''))
+  const rate = Math.max(document.documentElement.clientHeight, document.documentElement.clientWidth) / distance
+  document.querySelector(":root").style.setProperty('--centerDistance', document.documentElement.clientHeight / 2 - (dom.parentElement.parentElement.offsetTop + dom.parentElement.offsetTop + dom.clientHeight / 2
+  ) + 'px')
+  document.querySelector(":root").style.setProperty('--largeRate', rate)
+
+  // 可视区高度
+  const clientHeight =
+    document.documentElement.clientHeight || document.body.clientHeight
+  // 滚动条总高度
+  const scrollHeight =
+    document.getElementsByTagName('main')[0].scrollHeight
+
+  let down = true // 定义一个标志，当滚轮向下滚时，执行一些操作
+  let lastFlag = false // 上一次滚动时间
+  let timer = null
+  function wheel(e) {
+    if (!lastFlag) {
+      lastFlag = true
+      // 距顶部
+      const scrollTop =
+        document.documentElement.scrollTop
+      e = e || window.event;
+      down = e.wheelDelta ? e.wheelDelta < 0 : e.detail > 0;
+      turnPage(scrollTop)
+      return
+    }
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      lastFlag = false
+    }, 100)
+  }
+
+  let startClientY, threshold = 20 // 触摸时的位置，手指触摸的阈值
+
+  function touchstart(e) {
+    startClientY = e.changedTouches[0].clientY
+  }
+
+  function touchend(e) {
+    if (!lastFlag) {
+      lastFlag = true
+      // 距顶部
+      const scrollTop =
+        document.documentElement.scrollTop
+      const temp = startClientY - e.changedTouches[0].clientY
+      if (Math.abs(temp) <= threshold) return
+      down = temp > 0
+      turnPage(scrollTop)
+      return
+    }
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      lastFlag = false
+    }, 100)
+  }
+
+  const pagination = { _index: 0 }
+  Object.defineProperty(pagination, 'index', {
+    set(value) {
+      // 清空之前的索引样式
+      document.querySelector(`.control-box>li:nth-of-type(${this._index + 1})>div`).classList.remove('control-active')
+      const boxSecond = Number(getComputedStyle(root).getPropertyValue("--boxSecond").replace('s', '')) * 1000
+      if (pagination.index < value) {
+        // 下一页
+        switch (pagination.index) {
+          case 0:
+            document.documentElement.scrollTo(0, 0)
+            // 打开盒子并展示盒子最上页
+            setTimeout(() => {
+              document.getElementsByTagName('main')[0].style = "overflow: hidden;height: 100vh;"
+              document.getElementsByClassName('gift')[0].classList.remove('close-gift', 'small-gift')
+              document.getElementsByClassName('gift')[0].classList.add('open-gift', 'large-gift')
+            }, boxSecond)
+            break
+          case 1:
+            // 右移
+            document.getElementsByClassName('gift')[0].classList.add('gift-right')
+            break
+          case 2:
+            // 左下
+            document.getElementsByClassName('gift')[0].classList.add('gift-left-bottom')
+            break
+          case 3:
+            // 左上
+            document.getElementsByClassName('gift')[0].classList.add('gift-left-top')
+            break
+          case 4:
+            // 右上
+            document.getElementsByClassName('gift')[0].classList.add('gift-right-top')
+            break
+          case 5:
+            // 上
+            document.getElementsByClassName('gift')[0].classList.add('gift-top')
+            document.getElementsByClassName('article-end')[0].style = "opacity:1"
+            document.getElementById('content').style = "opacity:0"
+            break
+          case 6:
+            // 关闭
+            document.getElementsByClassName('gift-box')[0].classList.add('gift-box-translate')
+            document.getElementsByClassName('gift')[0].classList.add('small-gift', 'close-gift')
+            document.getElementsByClassName('gift')[0].classList.remove('large-gift', 'gift-right', 'gift-left-bottom', 'gift-left-top', 'gift-right-top', 'gift-top')
+            setTimeout(() => {
+              document.getElementsByClassName('gift-title-content')[0].classList.add('gift-title-content-last')
+            }, boxSecond)
+            break
+        }
+      } else {
+        // 上一页
+        switch (pagination.index) {
+          case 1:
+            setTimeout(() => {
+              // 需要保留open-gift动画
+              document.getElementsByClassName('gift')[0].classList.remove('large-gift')
+              document.getElementsByClassName('gift')[0].classList.add('small-gift')
+              setTimeout(() => {
+                document.documentElement.scrollTo({
+                  top: document.documentElement.scrollHeight - document.documentElement.clientHeight,
+                  left: 0,
+                  behavior: "smooth"
+                })
+                document.getElementsByClassName('gift')[0].classList.add('close-gift')
+              }, boxSecond)
+              document.getElementsByTagName('main')[0].style = 'overflow: hidden auto;height: auto;'
+            }, boxSecond)
+            break
+          case 2:
+            document.getElementsByClassName('gift')[0].classList.remove('gift-right')
+            break
+          case 3:
+            document.getElementsByClassName('gift')[0].classList.remove('gift-left-bottom')
+            break
+          case 4:
+            document.getElementsByClassName('gift')[0].classList.remove('gift-left-top')
+            break
+          case 5:
+            document.getElementsByClassName('gift')[0].classList.remove('gift-right-top')
+            break
+          case 6:
+            document.getElementsByClassName('gift')[0].classList.remove('gift-top')
+            document.getElementById('content').style = ""
+            document.getElementsByClassName('article-end')[0].style = ""
+            document.getElementsByClassName('gift-title-content')[0].classList.remove('gift-title-content-last')
+            break
+          case 7:
+            document.getElementsByClassName('gift-box')[0].classList.remove('gift-box-translate')
+            document.getElementsByClassName('gift')[0].classList.remove('close-gift', 'small-gift')
+            document.getElementsByClassName('gift')[0].classList.add('open-gift', 'large-gift', 'gift-right', 'gift-left-bottom', 'gift-left-top', 'gift-right-top', 'gift-top')
+            document.getElementsByTagName('main')[0].style = "overflow: hidden;height: 100vh;"
+            break
+        }
+      }
+      this._index = value < 0 ? 0 : (value > 7 ? 7 : value)
+      document.querySelector(`.control-box>li:nth-of-type(${this._index + 1})>div`).classList.add('control-active')
+    },
+    get() {
+      return this._index
+    }
+  })
+
+  function turnPage(scrollTop) {
+    if (!pagination.index && !isFirstEnd) return
+    if (down) {
+      // 没有触底不做向下翻页操作
+      if (!pagination.index && scrollTop + clientHeight + threshold < scrollHeight) return
+      if (pagination.index >= 7) return
+      console.log('向下翻页---------')
+      pagination.index++
+    } else {
+      // 没有到顶不做向上翻页操作
+      if (pagination.index === 7 && scrollTop > threshold) return
+      if (pagination.index <= 0) return
+      console.log('向上翻页++++++++++')
+      pagination.index--
+    }
+  }
+
+  // c = element to scroll to or top position in pixels
+  // e = duration of the scroll in ms, time scrolling
+  // d = (optative) ease function. Default easeOutCuaic
+  function scrollTo(c, e, d) {
+    d || (d = easeOutCuaic);
+    var a = document.documentElement;
+    if (0 === a.scrollTop) {
+      var b = a.scrollTop;
+      ++a.scrollTop; a = b + 1 === a.scrollTop-- ? a : document.body
+    }
+    b = a.scrollTop; 0 >= e || ("object" === typeof b && (b = b.offsetTop),
+      "object" === typeof c && (c = c.offsetTop), function (a, b, c, f, d, e, h) {
+        function g() {
+          0 > f || 1 < f || 0 >= d ? a.scrollTop = c : (a.scrollTop = b - (b - c) * h(f),
+            f += d * e, setTimeout(g, e))
+        } g()
+      }(a, b, c, 0, 1 / e, 20, d))
+  };
+  function easeOutCuaic(t) { t--; return t * t * t + 1; }
+}
+
+window.onload = initAni()
