3e6baf92a48f682e57bf763c0be7e7d4d937d9e5
“wangzhuohua” <506980247@qq.com>
2023-03-10 15:01:48 +0800

    feat:修复兼容性问题

--- static/index.js
+++ static/index.js
@@ -286,7 +286,7 @@ function event() {
   })
 
   function turnPage(scrollTop) {
-    // if (!pagination.index && !isFirstEnd) return
+    if (!pagination.index && !isFirstEnd) return
     if (down) {
       // 没有触底不做向下翻页操作
       if (!pagination.index && scrollTop + clientHeight + threshold < scrollHeight) return
